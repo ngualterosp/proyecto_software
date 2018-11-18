6b893ca10b69da43077d6a0eb9386ba04db368d1
Leonardo Luque <lluqueg@unbosque.edu.co>
2018-11-18 10:18:24 -0500

    LE puse un dos

--- inicioAdmin.php
+++ inicioAdmin.php
@@ -146,7 +146,7 @@ session_start();
               <span aria-hidden="true">×</span>
             </button>
           </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session 2.</div>
           <div class="modal-footer">
             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
             <a class="btn btn-primary" href="login.html">Logout</a>
