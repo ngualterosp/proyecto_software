0356aa852e519e522dc3f95cb3767850eec5b001
Nicolas Gualteros <ngualterosp@unbosque.edu.co>
2018-11-20 12:20:16 -0500

    Live

---
+++ js/jslive.js
@@ -0,0 +1,21 @@
+(function(){
+  var video = document.getElementById('video'),
+    vendorUrl = window.URL || window.webkitURL;
+
+  navigator.getMedia = navigator.getUserMeida ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
+
+            //capturar video
+      navigator.getMedia({
+        video: true,
+        audio: false
+      }, function(stream){
+        video.src = vendorUrl.createObjectURL(stream);
+        video.play();
+      }, function(error) {
+          //si un error ocurre
+          // error.code
+      });
+})();
---
+++ live.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>LIVE</title>
+  <link rel="stylesheet" href="css/csslive.css">
+</head>
+
+<body>
+
+  <div class="booth">
+    <video id="video" width="400" height="300" autoplay> </video>
+  </div>
+  <script src="js/jslive.js"></script>
+</body>
+</html>
