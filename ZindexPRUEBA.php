7ae5ce5c092899755a43a6e7c7b1782cbf3526ca
Nicolas Gualteros <ngualterosp@unbosque.edu.co>
2018-11-21 16:22:17 -0500

    es una prueba

---
+++ ZindexPRUEBA.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>indez</title>
+</head>
+<body>
+  <form action="ZuploadPRUEBA.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="image"/>
+    <input type="submit" name="submit" value="Subir Imagen"/>
+  </form>
+</body>
+</html>
