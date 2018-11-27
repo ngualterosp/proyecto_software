e5e4f053bef33c8d715d7fb6b77e66e67b8e5613
Unknown <lluqueg@unbosque.edu.co>
2018-11-27 02:47:31 -0500

    Arreglo tienda tickets

--- Modelos/gira/administrar_gira.php
+++ Modelos/gira/administrar_gira.php
@@ -9,27 +9,7 @@ $gira= new Gira();
 
 	// si el elemento insertar no viene nulo llama al crud e inserta un noticia
 	if (isset($_POST['submit'])) {
-		if(isset($_POST["insert"])){
-		    $check = getimagesize($_FILES["image"]["tmp_name"]);
-		    if($check !== false){
-		        $image = $_FILES['image']['tmp_name'];
-		        $imgContent = addslashes(file_get_contents($image));
-						$db = Db::conectar();
-						if($db->connect_error){
-								die("Connection failed: " . $db->connect_error);
-						}
-
-						//Insert image content into database
-						$insert = $db->query("INSERT into gira (ruta_imagen) VALUES ('$imgContent')");
-						if($insert){
-								echo "File uploaded successfully.";
-						}else{
-								echo "File upload failed, please try again.";
-						}
-				}else{
-						echo "Please select an image file to upload.";
-				}
-		}
+		
 
 		$gira->setNombre($_POST['nom_gira']);
 		$gira->setDescripcion($_POST['descripcion_gira']);
